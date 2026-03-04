781ab50c470a794225a4b7b606f64c57822618b3
BoorBoorBoor <easy192736@gmail.com>
2026-03-04 14:11:33 +0300

    IO

--- example/array.py
+++ example/array.py
@@ -1,15 +1,24 @@
-arr = list()
-n = int(input("Enter number of elements: "))
-print("Enter elements:")
-i = 0
-while (i < n):
-    tmp = int(input(""))
-    arr.append(tmp)
-    i += 1
+N_MAX = 15
 
-print("Array:")
-i = 0
-while (i < n):
-    print(arr[i], end = " ")
-    i += 1
-print("")
+def read_array():
+    arr = list()
+    n = int(input("Enter number of elements: "))
+    print("Enter elements:")
+    i = 0
+    while (i < n):
+        tmp = int(input(""))
+        arr.append(tmp)
+        i += 1
+    return arr
+
+def print_array(arr):
+    n = len(arr)
+    print("Array:")
+    i = 0
+    while (i < n):
+        print(arr[i], end = " ")
+        i += 1
+    print("")
+
+my_arr = read_array()
+print_array(my_arr)
